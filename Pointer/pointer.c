bb852f4b36ee770ea9a24ff6637b65920e2b2aec
samyamx <samyam00349@gmail.com>
2026-07-07 02:26:53 +0545

    pointer added

---
+++ Pointer/pointer.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+
+    int arr[] = {10,20,30,40,50};
+
+    int *ptr = arr;
+
+    printf("%d\n", *ptr);
+
+    ptr++;
+
+    printf("%d\n", *ptr);
+
+    ptr++;
+
+    printf("%d\n", *ptr);
+
+    return 0;
+}
