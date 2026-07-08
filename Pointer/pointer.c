eb532b60106ac404ddcd90c74b978673518717fa
samyamx <samyam00349@gmail.com>
2026-07-08 21:12:58 +0545

    arrray two

--- Pointer/pointer.c
+++ Pointer/pointer.c
@@ -4,9 +4,9 @@ int main()
 {
     int arr[5]={10,20,30,40,50};
 
-    printf("%d\n",arr[0]);
-    printf("%d\n",arr[1]);
-    printf("%d\n",arr[2]);
+    arr[2]=100;
+
+    printf("%d",arr[2]);
 
     return 0;
 }
