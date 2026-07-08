3f55f9e364319d3a08be865d222425e08356809c
samyamx <samyam00349@gmail.com>
2026-07-08 21:19:20 +0545

    3

--- Pointer/pointer.c
+++ Pointer/pointer.c
@@ -4,16 +4,10 @@ int main()
 {
     int arr[5]={10,20,30,40,50};
 
-    int sum=0;
-
-    for(int i=0;i<5;i++)
+    for(int i=4;i>=0;i--)
     {
-        sum+=arr[i];
+        printf("%d ",arr[i]);
     }
-
-    float average=(float)sum/5;
-
-    printf("Average=%.2f",average);
 
     return 0;
 }
