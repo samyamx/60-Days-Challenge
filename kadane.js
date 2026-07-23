6cf42a473c561c53b495dfdbd4d03aac9ee54ea2
samyamx <samyam00349@gmail.com>
2026-07-23 23:20:34 +0545

    kadaneQ3

--- kadane.js
+++ kadane.js
@@ -1,22 +1,34 @@
 function maxSubArray(nums) {
 
-    let currentSum = 0;
-    let maxSum = -Infinity;
+    let currentSum = nums[0];
+    let maxSum = nums[0];
 
-    for (let num of nums) {
+    let start = 0;
+    let end = 0;
+    let tempStart = 0;
 
-        currentSum += num;
+    for (let i = 1; i < nums.length; i++) {
+
+        if (nums[i] > currentSum + nums[i]) {
+            currentSum = nums[i];
+            tempStart = i;
+        } else {
+            currentSum += nums[i];
+        }
 
         if (currentSum > maxSum) {
             maxSum = currentSum;
-        }
-
-        if (currentSum < 0) {
-            currentSum = 0;
+            start = tempStart;
+            end = i;
         }
     }
 
-    return maxSum;
+    return {
+        maxSum,
+        subarray: nums.slice(start, end + 1)
+    };
 }
 
-console.log(maxSubArray([-2,6,-3,-4,-1,8,1,-5,4]));
+const result = maxSubArray([-2,1,-3,4,-1,2,1,-5,4]);
+
+console.log(result);
