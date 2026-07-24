64b55cb27fe4cabb6b5b50a294a26704be9b8b25
samyamx <samyam00349@gmail.com>
2026-07-24 23:33:06 +0545

    kadaneQ4

--- kadane.js
+++ kadane.js
@@ -1,34 +1,22 @@
-function maxSubArray(nums) {
+function kadane(nums) {
 
     let currentSum = nums[0];
     let maxSum = nums[0];
 
-    let start = 0;
-    let end = 0;
-    let tempStart = 0;
+    console.log("Index\tNumber\tCurrentSum\tMaxSum");
+
+    console.log(`0\t${nums[0]}\t${currentSum}\t\t${maxSum}`);
 
     for (let i = 1; i < nums.length; i++) {
 
-        if (nums[i] > currentSum + nums[i]) {
-            currentSum = nums[i];
-            tempStart = i;
-        } else {
-            currentSum += nums[i];
-        }
+        currentSum = Math.max(nums[i], currentSum + nums[i]);
 
-        if (currentSum > maxSum) {
-            maxSum = currentSum;
-            start = tempStart;
-            end = i;
-        }
+        maxSum = Math.max(maxSum, currentSum);
+
+        console.log(`${i}\t${nums[i]}\t${currentSum}\t\t${maxSum}`);
     }
 
-    return {
-        maxSum,
-        subarray: nums.slice(start, end + 1)
-    };
+    console.log("\nMaximum Sum =", maxSum);
 }
 
-const result = maxSubArray([-2,1,-3,4,-1,2,1,-5,4]);
-
-console.log(result);
+kadane([-2,1,-3,4,-1,2,1,-5,4]);
