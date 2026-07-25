17ebf660ea55d6a1e9dc63ae6b72b2af154d5325
samyamx <samyam00349@gmail.com>
2026-07-25 23:49:10 +0545

    plant js

--- plant.js
+++ plant.js
@@ -11,3 +11,23 @@ addButtons.forEach(button => {
     });
 });
 
+const search = document.getElementById("search");
+const cards = document.querySelectorAll(".card");
+
+search.addEventListener("keyup", () => {
+
+    const value = search.value.toLowerCase();
+
+    cards.forEach(card => {
+
+        const plantName = card.dataset.name.toLowerCase();
+
+        if (plantName.includes(value)) {
+            card.style.display = "block";
+        } else {
+            card.style.display = "none";
+        }
+
+    });
+
+});
