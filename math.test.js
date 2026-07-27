c0289b34064b381de12ff107e89444998875a93d
samyamx <samyam00349@gmail.com>
2026-07-27 22:49:16 +0545

    plant css counter

--- math.test.js
+++
@@ -1,8 +0,0 @@
-import { describe, test, expect } from "vitest";
-import { add } from "./math";
-
-describe("Addition Function", () => {
-  test("adds two numbers", () => {
-    expect(add(2, 3)).toBe(5);
-  });
-});
